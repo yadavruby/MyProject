cbb8e648b8f829ab162f596026ca314c16285611
Ruby <Ruby @DESKTOP-V5H6KLI.attlocal.net>
2018-02-13 17:46:00 -0600

    Type commit

---
+++ Practice/src/JavaPractice/Practice1.java
@@ -0,0 +1,23 @@
+package JavaPractice;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*This step prints in the output
+ System.out.println("I started learning Java");
+		
+Adding two numbers
+int a = 2;
+int b =3;
+int Sum = a+b;
+System.out.println(Sum);
+System.out.println("Sum is " + Sum);*/
+
+System.out.println("I navigated to home page");
+
+
+
+	}
+
+}
---
+++ Practice/src/JavaPractice/Practice2.java
@@ -0,0 +1,11 @@
+package JavaPractice;
+
+public class Practice2 {
+
+public void ValidateHeader()
+{	
+System.out.println("Header links validated");
+
+}
+
+}
